4b177727f8fe414ead1f9b1cdfb8894fed1df245
elios cama <elios.cama@gmail.com>
2023-12-22 17:37:54 +0100

    created the bottom sheet with camera flow

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,15 +1,45 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:pokedex/presentation/widget/pokemon_grid.dart';
 
 import '../application/pokemon_services.dart';
 import '../application/search_delegate.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends ConsumerState<HomePage> {
+  CameraController? _cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isNotEmpty) {
+      final camera = cameras.first;
+      _cameraController = CameraController(camera, ResolutionPreset.medium);
+      await _cameraController!.initialize();
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final pokemonNotifier = ref.watch(pokemonNotifierProvider.notifier);
     final pokemonNames = pokemonNotifier.getPokemonNames();
     return Scaffold(
@@ -94,6 +124,51 @@ class HomePage extends ConsumerWidget {
             child: PokemonGrid(),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        backgroundColor: const Color(0xFFC8614E),
+        onPressed: () async {
+          if (await Permission.camera.request().isGranted) {
+            if (mounted) {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                builder: (BuildContext context) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFF8E0DE),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: 400,
+                    padding: const EdgeInsets.all(20),
+                    child: SizedBox(
+                      height: 300,
+                      child: _cameraController != null &&
+                              _cameraController!.value.isInitialized
+                          ? CameraPreview(_cameraController!)
+                          : const Center(child: CircularProgressIndicator()),
+                    ),
+                  );
+                },
+              );
+            }
+          }
+        },
+        child: const Icon(
+          Icons.camera_alt_outlined,
+          color: Colors.white,
+        ),
       ),
     );
   }
